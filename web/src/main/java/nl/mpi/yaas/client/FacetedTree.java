2ebb0a454831bc763a8482d3784c5d5d2e77a974
Peter Withers <Peter.Withers@mpi.nl>
2013-07-16 12:52:03 +0000

    refs #2927 Added the faceted tree branch selection menu.

--- web/src/main/java/nl/mpi/yaas/client/FacetedTree.java
+++ web/src/main/java/nl/mpi/yaas/client/FacetedTree.java
@@ -30,21 +30,41 @@ import nl.mpi.yaas.common.data.MetadataFileType;
  *
  * @author Peter Withers <peter.withers@mpi.nl>
  */
-public class FacetedTree extends VerticalPanel implements ScheduledCommand {
+public class FacetedTree extends VerticalPanel {
 
     final private SearchOptionsServiceAsync searchOptionsService;
-    final private ArrayList<MetadataFileType> selectedFacetets = new ArrayList<MetadataFileType>();
+    final private ArrayList<MetadataFileType[]> selectedFacets = new ArrayList<MetadataFileType[]>();
+    final private MenuBar menuBar;
 
     public FacetedTree(SearchOptionsServiceAsync searchOptionsService) {
         this.searchOptionsService = searchOptionsService;
-        MenuBar menu = new MenuBar();
-        menu.addItem("<please select a facet>", getMenuItems());
-        add(menu);
+        menuBar = new MenuBar();
+        updateRootMenus(menuBar);
+        add(menuBar);
     }
 
-    private MenuBar getMenuItems() {
+    private void updateRootMenus(MenuBar menuBar) {
+        menuBar.clearItems();
+        int menuIndex = 0;
+        for (MetadataFileType[] type : selectedFacets) {
+            String labelString = "";
+            if (type[0].getType() != null && type[1].getPath() != null) {
+                labelString = type[0].getType() + " / " + type[1].toString();
+            } else {
+                labelString += type[1].toString();
+            }
+            menuBar.addItem(labelString, getMenuItems(menuIndex++));
+        }
+        if (selectedFacets.isEmpty()) {
+            menuBar.addItem("<please select a facet>", getMenuItems(menuIndex++));
+        } else {
+            menuBar.addItem("<add another facet>", getMenuItems(menuIndex++));
+        }
+    }
+
+    private MenuBar getMenuItems(int menuIndex) {
         MenuBar facetMenu = new MenuBar(true);
-        loadTypeOptions(facetMenu, null);
+        loadTypeOptions(facetMenu, null, menuIndex);
         return facetMenu;
     }
 
@@ -52,7 +72,7 @@ public class FacetedTree extends VerticalPanel implements ScheduledCommand {
         Window.alert("You selected a menu item!");
     }
 
-    private void loadTypeOptions(final MenuBar typeMenu, final MetadataFileType type) {
+    private void loadTypeOptions(final MenuBar typeMenu, final MetadataFileType type, final int menuIndex) {
         searchOptionsService.getTypeOptions(type, new AsyncCallback<MetadataFileType[]>() {
             public void onFailure(Throwable caught) {
                 Window.alert(caught.getMessage());
@@ -63,14 +83,22 @@ public class FacetedTree extends VerticalPanel implements ScheduledCommand {
                     for (MetadataFileType facetType : result) {
                         MenuBar menu = new MenuBar(true);
                         typeMenu.addItem(facetType.toString(), menu);
-                        loadFacetOptions(menu, facetType);
+                        loadFacetOptions(menu, facetType, menuIndex);
                     }
+                }
+                if (selectedFacets.size() > menuIndex) {
+                    typeMenu.addItem("<Remove>", new ScheduledCommand() {
+                        public void execute() {
+                            selectedFacets.remove(menuIndex);
+                            updateRootMenus(menuBar);
+                        }
+                    });
                 }
             }
         });
     }
 
-    private void loadFacetOptions(final MenuBar facetMenu, final MetadataFileType type) {
+    private void loadFacetOptions(final MenuBar facetMenu, final MetadataFileType type, final int menuIndex) {
         searchOptionsService.getPathOptions(type, new AsyncCallback<MetadataFileType[]>() {
             public void onFailure(Throwable caught) {
                 Window.alert(caught.getMessage());
@@ -78,10 +106,17 @@ public class FacetedTree extends VerticalPanel implements ScheduledCommand {
 
             public void onSuccess(MetadataFileType[] result) {
                 if (result != null && result.length > 0) {
-                    for (MetadataFileType facetType : result) {
-//                        MenuBar menu = new MenuBar(true);
-                        facetMenu.addItem(facetType.toString(), FacetedTree.this);
-//                        loadFacetOptions(facetMenu, facetType);
+                    for (final MetadataFileType facetType : result) {
+                        facetMenu.addItem(facetType.toString(), new ScheduledCommand() {
+                            public void execute() {
+                                if (selectedFacets.size() <= menuIndex) {
+                                    selectedFacets.add(new MetadataFileType[]{type, facetType});
+                                } else {
+                                    selectedFacets.set(menuIndex, new MetadataFileType[]{type, facetType});
+                                }
+                                updateRootMenus(menuBar);
+                            }
+                        });
                     }
                 }
             }
