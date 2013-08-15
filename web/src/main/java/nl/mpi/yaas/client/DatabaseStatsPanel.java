ebdace6d31c245dfdef00cda8cdaa74a5b16c097
Peter Withers <Peter.Withers@mpi.nl>
2013-08-15 13:27:23 +0000

    refs #3225 Added database selection to the GWT user interface so that other crawls can be viewed such as CMDIfied CGN and others.

--- web/src/main/java/nl/mpi/yaas/client/DatabaseStatsPanel.java
+++ web/src/main/java/nl/mpi/yaas/client/DatabaseStatsPanel.java
@@ -6,7 +6,10 @@ package nl.mpi.yaas.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import nl.mpi.yaas.common.data.DatabaseStats;
 
 /**
@@ -16,16 +19,46 @@ import nl.mpi.yaas.common.data.DatabaseStats;
  */
 public class DatabaseStatsPanel extends VerticalPanel {
 
+    private final ListBox databaseListBox = new ListBox();
+    private final SearchOptionsServiceAsync searchOptionsService;
+    private final DataNodeTree dataNodeTree;
+    private static final Logger logger = Logger.getLogger("");
+    private static final String FAILED_TO_GET_THE_DATABASE_LIST = "Failed to get the database list";
+    private static final String FAILED_TO_GET_THE_DATABASE_STATISTICS = "Failed to get the database statistics";
 //    final private SearchOptionsServiceAsync searchOptionsService;
 //    final private DataNodeTree dataNodeTree;
+
     public DatabaseStatsPanel(SearchOptionsServiceAsync searchOptionsService, final DataNodeTree dataNodeTree) {
-//        this.searchOptionsService = searchOptionsService;
-//        this.dataNodeTree = dataNodeTree;
-//        DatabaseStatsPanel.this.add(new Label("Getting Database Stats"));
+        this.searchOptionsService = searchOptionsService;
+        this.dataNodeTree = dataNodeTree;
+        add(databaseListBox);
+        getDbList();
+        updateDbStats();
+    }
+
+    private void getDbList() {
+        searchOptionsService.getDatabaseList(new AsyncCallback<String[]>() {
+            public void onFailure(Throwable caught) {
+                DatabaseStatsPanel.this.add(new Label(FAILED_TO_GET_THE_DATABASE_LIST));
+                logger.log(Level.SEVERE, FAILED_TO_GET_THE_DATABASE_LIST, caught);
+            }
+
+            public void onSuccess(String[] result) {
+                for (String databaseName : result) {
+                    databaseListBox.addItem(databaseName);
+                }
+            }
+        });
+    }
+
+    private void updateDbStats() {
+        //        this.searchOptionsService = searchOptionsService;
+        //        this.dataNodeTree = dataNodeTree;
+        //        DatabaseStatsPanel.this.add(new Label("Getting Database Stats"));
         searchOptionsService.getDatabaseStats(new AsyncCallback<DatabaseStats>() {
             public void onFailure(Throwable caught) {
-                DatabaseStatsPanel.this.add(new Label("Failure"));
-                DatabaseStatsPanel.this.add(new Label(caught.getMessage()));
+                DatabaseStatsPanel.this.add(new Label(FAILED_TO_GET_THE_DATABASE_STATISTICS));
+                logger.log(Level.SEVERE, FAILED_TO_GET_THE_DATABASE_STATISTICS, caught);
             }
 
             public void onSuccess(DatabaseStats result) {
@@ -34,7 +67,7 @@ public class DatabaseStatsPanel extends VerticalPanel {
                 DatabaseStatsPanel.this.add(new Label("Missing Documents Count: " + result.getMisingDocumentsCount()));
                 DatabaseStatsPanel.this.add(new Label("Duplicate Documents Count: " + result.getDuplicateDocumentsCount()));
                 DatabaseStatsPanel.this.add(new Label("Query time: " + result.getQueryTimeMS() + "ms"));
-//                final YaasTreeItem yaasTreeItem = new YaasTreeItem();
+                //                final YaasTreeItem yaasTreeItem = new YaasTreeItem();
                 dataNodeTree.addResultsToTree(result.getRootDocumentsIDs());
             }
         });
