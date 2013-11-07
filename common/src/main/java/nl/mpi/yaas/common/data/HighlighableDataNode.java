fbf342c94f4da0c0ec424f1c3d3970e4714dd622
Peter Withers <Peter.Withers@mpi.nl>
2013-11-07 10:00:17 +0000

    Added highlights to nodes that contain the fields matching the users search.

--- common/src/main/java/nl/mpi/yaas/common/data/HighlighableDataNode.java
+++ common/src/main/java/nl/mpi/yaas/common/data/HighlighableDataNode.java
@@ -1,19 +1,20 @@
-/*
- * Copyright (C) 2013 Max Planck Institute for Psycholinguistics
+/**
+ * Copyright (C) 2013 The Language Archive, Max Planck Institute for
+ * Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.yaas.common.data;
 
@@ -21,7 +22,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
-import nl.mpi.flap.model.ModelException;
 import nl.mpi.flap.model.SerialisableDataNode;
 
 /**
@@ -31,7 +31,7 @@ import nl.mpi.flap.model.SerialisableDataNode;
  */
 public class HighlighableDataNode extends SerialisableDataNode implements Serializable {
 
-    private List<DataNodeHighlight> highlights;
+    private List<DataNodeHighlight> highlights = new ArrayList<DataNodeHighlight>();
 
     public List<DataNodeHighlight> getHighlights() {
         return highlights;
@@ -40,20 +40,5 @@ public class HighlighableDataNode extends SerialisableDataNode implements Serial
     @XmlElement(name = "Highlight")
     public void setHighlights(List<DataNodeHighlight> highlighedLinks) {
         this.highlights = highlighedLinks;
-    }
-
-    public List<DataNodeHighlight> getHighlightsForNode(SerialisableDataNode dataNode) throws ModelException {
-        final String id = dataNode.getID();
-        return getHighlightsForNode(id);
-    }
-
-    public List<DataNodeHighlight> getHighlightsForNode(String id) throws ModelException {
-        List<DataNodeHighlight> returnList = new ArrayList<DataNodeHighlight>();
-        for (DataNodeHighlight highlight : highlights) {
-            if (highlight.getDataNodeId().equals(id)) {
-                returnList.add(highlight);
-            }
-        }
-        return returnList;
     }
 }
