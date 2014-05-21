0dbf09a9eb588b8a21d8a91662847638a3727290
Peter Withers <Peter.Withers@mpi.nl>
2014-05-21 12:50:04 +0000

    refs #3851 Created a prototype mobile application to verify that the use of HTML5+PhoneGap will be adequate for the task.

---
+++ annotator/src/main/java/nl/mpi/yaas/annotator/client/AnnotationData.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.mpi.yaas.annotator.client;
+
+/**
+ * @since Jan 31, 2014 10:04 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+public class AnnotationData {
+
+    private double inTime = 0;
+    private double outTime = 0;
+    private String annotationHtml = "";
+
+    public AnnotationData() {
+    }
+
+    public AnnotationData(double inTime, double outTime, String annotationHtml) {
+        this.inTime = inTime;
+        this.outTime = outTime;
+        this.annotationHtml = annotationHtml;
+    }
+
+    public double getInTime() {
+        return inTime;
+    }
+
+    public void setInTime(double inTime) {
+        this.inTime = inTime;
+    }
+
+    public double getOutTime() {
+        return outTime;
+    }
+
+    public void setOutTime(double outTime) {
+        this.outTime = outTime;
+    }
+
+    public String getAnnotationHtml() {
+        return annotationHtml;
+    }
+
+    public void setAnnotationHtml(String annotationHtml) {
+        this.annotationHtml = annotationHtml;
+    }
+}
---
+++ annotator/src/main/java/nl/mpi/yaas/annotator/client/annotator.java
@@ -0,0 +1,87 @@
+package nl.mpi.yaas.annotator.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class annotator implements EntryPoint {
+
+    /**
+     * The message displayed to the user when the server cannot be reached or
+     * returns an error.
+     */
+    private static final String SERVER_ERROR = "An error occurred while "
+            + "attempting to contact the server. Please check your network "
+            + "connection and try again.";
+
+    private final Messages messages = GWT.create(Messages.class);
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        final Label errorLabel = new Label();
+
+        // Add the nameField and sendButton to the RootPanel
+        // Use RootPanel.get() to get the entire body element
+        RootPanel.get("errorLabelContainer").add(errorLabel);
+
+        // Create the popup dialog box
+        final DialogBox dialogBox = new DialogBox();
+        dialogBox.setText("Tools Pallet");
+        dialogBox.setAnimationEnabled(true);
+        final Label textToServerLabel = new Label();
+        final HTML serverResponseLabel = new HTML();
+        VerticalPanel dialogVPanel = new VerticalPanel();
+        dialogVPanel.addStyleName("dialogVPanel");
+        dialogVPanel.add(new HTML("<b>...</b>"));
+        dialogVPanel.add(textToServerLabel);
+        dialogVPanel.add(new HTML("<br><b>...</b>"));
+        dialogVPanel.add(serverResponseLabel);
+        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+        dialogBox.setWidget(dialogVPanel);
+
+        RootPanel.get("toolspallet").add(dialogBox);
+        ////////////
+        // Create the text area and toolbar
+        final AnnotationEditor annotationEditor = new AnnotationEditor();
+//        final VideoPanel videoPanel = new VideoPanel("Screen Shot 2014-01-16 at 7.46.33 PM.png", "SSL_LM_lex_b.mp4", "video/mp4");
+        final VideoPanel videoPanel = new VideoPanel("images/sidebarl.png"); //, "http://hdl.handle.net/1839/00-0000-0000-000D-AB50-0", "video/m4a"
+//        videoPanel.addSource("http://download.blender.org/peach/trailer/trailer_480p.mov", "video/mov");
+//        videoPanel.addSource("http://download.blender.org/peach/trailer/trailer_400p.ogg", "video/ogg");
+//        videoPanel.addSource("http://mirror.cessen.com/blender.org/peach/trailer/trailer_iphone.m4v", "video/m4v");
+//        videoPanel.add(new HTML("<p>Video courtesy of <a href=\"http://www.bigbuckbunny.org/\" target=\"_blank\">Big Buck Bunny</a>.</p>"));
+        videoPanel.addSource("http://www.w3schools.com/html/mov_bbb.mp4", "video/mp4");
+        videoPanel.addSource("http://www.w3schools.com/html/mov_bbb.ogg", "video/ogg");
+        RootPanel.get("videopanel").add(videoPanel);
+        final AnnotationPanel annotationPanel = new AnnotationPanel(annotationEditor);
+        Controller handler = new Controller(annotationPanel, annotationEditor, videoPanel, errorLabel, textToServerLabel, serverResponseLabel);
+        annotationPanel.setController(handler);
+//        annotationPanel.ensureDebugId("cwRichText-area");
+        annotationPanel.setSize("100%", "14em");
+//        RichTextToolbar toolbar = new RichTextToolbar(area);
+//        toolbar.ensureDebugId("cwRichText-toolbar");
+//        toolbar.setWidth("100%");
+//        final RichTextArea.Formatter formatter = richTextAarea.getFormatter();
+//        formatter.insertHTML("<style>\n"
+//                + "body {counter-reset:section;}\n"
+//                + "div:before\n"
+//                + "{\n"
+//                + "counter-increment:section;\n"
+//                + "content:\"Section \" counter(section) \". \";\n"
+//                + "}\n"
+//                + "</style>");
+
+        RootPanel.get("richtexteditor").add(annotationPanel);         ////////////////////
+
+        // Focus the cursor on the name field when the app loads
+        annotationPanel.setFocus(true);
+    }
+}
