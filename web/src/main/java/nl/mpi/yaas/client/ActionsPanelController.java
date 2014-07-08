fa9d0a8afb380d8455d40dec213eb5aff2ca05dd
Peter Withers <Peter.Withers@mpi.nl>
2014-07-08 15:27:31 +0000

    Added and updated the service locations and REST paths. Updated the node actions panel controller to support both CS2CMDI and Basex data sources. Made adjustments to support the missing URI and updated HDL format provided by CS2CMDI API.

--- web/src/main/java/nl/mpi/yaas/client/ActionsPanelController.java
+++ web/src/main/java/nl/mpi/yaas/client/ActionsPanelController.java
@@ -30,6 +30,7 @@ import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import nl.mpi.flap.model.ModelException;
@@ -50,6 +51,7 @@ public class ActionsPanelController implements HistoryListener {
     private final DatabaseInfo databaseInfo;
     private final SearchOptionsServiceAsync searchOptionsService;
     private SerialisableDataNode dataNode = null;
+    private DataNodeId nodeId = null;
     private final HistoryController historyController;
     final private RootPanel actionsTargetPanel;
     final private RootPanel detailsPanel;
@@ -69,47 +71,63 @@ public class ActionsPanelController implements HistoryListener {
     final private RootPanel loginTag;
     final private RootPanel logoutTag;
     final ServiceLocations serviceLocations = GWT.create(ServiceLocations.class);
+    private String errorMessage = null;
 
     public void historyChange() {
+        errorMessage = null;
         final List<DataNodeId> branchSelectionList = historyController.getHistoryData().getBranchSelection();
-        String nodeId = null;
-        if (dataNode != null) {
-            try {
-                nodeId = dataNode.getID();
-            } catch (ModelException exception) {
-                logger.info(exception.getMessage());
-            }
-        }
-        if (dataNode != null && nodeId != null && !branchSelectionList.isEmpty() && nodeId.equals(branchSelectionList.get(0).getIdString())) {
+//        if (dataNode != null) {
+//            try {
+//                nodeId = dataNode.getID();
+//            } catch (ModelException exception) {
+//                logger.info(exception.getMessage());
+//            }
+//        }
+        if (dataNode != null && nodeId != null && !branchSelectionList.isEmpty() && nodeId.equals(branchSelectionList.get(0))) {
             // todo: if we start supporting multiple selections then this will need to change
             // the current data node is already selected so no change needed.
         } else {
             final String databaseName = historyController.getDatabaseName();
             if (databaseName != null) {
                 final IconTableBase64 databaseIcons = databaseInfo.getDatabaseIcons(databaseName);
-                final DataNodeLoader dataNodeLoader = new DataNodeLoaderRpc(searchOptionsService, databaseIcons, databaseName);
+//                final DataNodeLoader dataNodeLoader = new DataNodeLoaderRpc(searchOptionsService, databaseIcons, databaseName);
+                final DataNodeLoader dataNodeLoader = new DataNodeLoaderJson(databaseName);
 //                logger.info("branchSelectionList.size():" + branchSelectionList.size());
                 if (branchSelectionList.isEmpty()) {
                     dataNode = null;
                     doNodeAction(historyController.getHistoryData().getNodeActionType());
                 } else {
+                    nodeId = branchSelectionList.get(0);
 //                    logger.info("branchSelectionList.size():" + branchSelectionList.size());
-//                    logger.info("branchSelectionList.get(0).getIdString():" + branchSelectionList.get(0).getIdString());
-                    dataNodeLoader.requestLoad(branchSelectionList, new DataNodeLoaderListener() {
+//                    logger.info("nodeId.getIdString():" + nodeId.getIdString());
+                    final DataNodeLoaderListener dataNodeLoaderListener = new DataNodeLoaderListener() {
                         public void dataNodeLoaded(List<SerialisableDataNode> dataNodeList) {
                             if (dataNodeList != null && !dataNodeList.isEmpty()) {
                                 dataNode = dataNodeList.get(0);
-                                logger.info(dataNode.getLabel());
+//                                logger.info(dataNode.getLabel());
+                                doNodeAction(historyController.getHistoryData().getNodeActionType());
                             } else {
-                                logger.warning("dataNodeLoaded but the resulting list was empty");
+                                showError("dataNodeLoaded but the resulting list was empty");
+                                logger.warning(errorMessage);
                             }
-                            doNodeAction(historyController.getHistoryData().getNodeActionType());
                         }
 
                         public void dataNodeLoadFailed(Throwable caught) {
                             logger.warning(caught.getMessage());
                         }
-                    });
+                    };
+                    // when getting the data from CS2DB we might get the url or handle or id, so we are trying to resolve this here before querying the server. 
+                    if (nodeId.getIdString().startsWith("hdl:")) {
+                        final List<String> hdlList = new ArrayList<String>();
+                        for (DataNodeId hdl : branchSelectionList) {
+                            hdlList.add(hdl.getIdString());
+                        }
+                        logger.info("requestLoadHdl");
+                        dataNodeLoader.requestLoadHdl(hdlList, dataNodeLoaderListener);
+                    } else {
+                        logger.info("requestLoad");
+                        dataNodeLoader.requestLoad(branchSelectionList, dataNodeLoaderListener);
+                    }
                 }
             }
         }
@@ -239,6 +257,8 @@ public class ActionsPanelController implements HistoryListener {
     }
 
     private void doNodeAction(final NodeActionType actionType) {
+//        logger.info("ActionsPanelController:doNodeAction");
+//        logger.info(actionType.name());
         if (detailsPanel != null) {
             detailsPanel.setVisible(false);
         }
@@ -247,39 +267,55 @@ public class ActionsPanelController implements HistoryListener {
         }
         actionsTargetPanel.clear();
         actionsTargetPanel.setVisible(true);
-//                try {
-        logger.info(actionType.name());
-        switch (actionType) {
-            case citation:
-                final CitationPanel citationPanel = new CitationPanel();
-                citationPanel.setDataNode(dataNode);
-                actionsTargetPanel.add(citationPanel);
-                break;
-            case details:
-                final MetadataDetailsPanel metadataDetailsPanel = new MetadataDetailsPanel();
+        if (errorMessage != null) {
+            showError(errorMessage);
+        } else {
+            try {
+                logger.info(actionType.name());
+                switch (actionType) {
+                    case citation:
+                        final CitationPanel citationPanel = new CitationPanel();
+                        citationPanel.setDataNode(dataNode);
+                        actionsTargetPanel.add(citationPanel);
+                        break;
+                    case details:
+                        final MetadataDetailsPanel metadataDetailsPanel = new MetadataDetailsPanel();
 //                actionsTargetPanel.add(metadataDetailsPanel);
-                detailsPanel.clear();
-                detailsPanel.add(metadataDetailsPanel);
-                metadataDetailsPanel.setDataNode(dataNode);
-                setDataNode(dataNode);
-                break;
-            case search:
-                doPanelAction(serviceLocations.yamsUrl());
+                        detailsPanel.clear();
+                        detailsPanel.add(metadataDetailsPanel);
+                        metadataDetailsPanel.setDataNode(dataNode);
+                        setDataNode(dataNode);
+                        break;
+                    case search:
+                        doPanelAction(serviceLocations.yamsUrl(dataNode.getURI()));
 //                actionsTargetPanel.add(new SearchPanel());
-                break;
-            case ams:
-                doPanelAction(serviceLocations.amsUrl());
-                break;
-            case rrs:
-                doPanelAction(serviceLocations.rrsUrl());
-                break;
-            case home:
-                setDataNode(null);
-                break;
+                        break;
+                    case ams:
+                        doPanelAction(serviceLocations.amsUrl(dataNode.getURI()));
+                        break;
+                    case rrs:
+                        doPanelAction(serviceLocations.rrsUrl());
+                        break;
+                    case home:
+                        setDataNode(null);
+                        break;
+                }
+            } catch (ModelException exception) {
+                showError(exception.getMessage());
+                logger.warning(exception.getMessage());
+            }
         }
-//                } catch (ModelException exception) {
-//                    logger.warning(exception.getMessage());
-//                } 
+    }
+
+    private void showError(String message) {
+        // show the error to the user
+//        final ErrorDataNode serialisableDataNode = new ErrorDataNode();
+//        serialisableDataNode.setLabel(errorMessage);
+//        dataNode = serialisableDataNode;
+        errorMessage = message;
+        detailsPanel.setVisible(true);
+        detailsPanel.clear();
+        detailsPanel.add(new Label(errorMessage));
     }
 
     private void addNodeAction(RootPanel rootPanel, final NodeActionType actionType) {
@@ -327,8 +363,8 @@ public class ActionsPanelController implements HistoryListener {
 
     private String getFormattedHandleLink(final String targetUrl) throws ModelException {
         if (dataNode != null) {
-            // this should be replaced with the use of gwt messages to add the parameter
-            return targetUrl.replace("{0}", dataNode.getURI());
+            // this should be replaced with the use of gwt messages to add the parameter, however the uri is only known some time after the targetUri is placed into a final instance.
+            return targetUrl.replace("<identifier>", dataNode.getURI());
         } else {
             return targetUrl;
         }
